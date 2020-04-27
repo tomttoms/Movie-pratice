4e50a65f5ccfed5b95725b9a919b7885d2a8e470
tomttoms <hosungx9x6@naver.com>
2020-04-27 23:32:07 +0900

    Updates

---
+++ precache-manifest.783026d9958120bf38fd8d24994ad40a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a618f7f726d23149221507f5102128fd",
+    "url": "/movie-pratice/index.html"
+  },
+  {
+    "revision": "fee3c20913153477f744",
+    "url": "/movie-pratice/static/css/main.25e285c4.chunk.css"
+  },
+  {
+    "revision": "efb729a049a51cc774db",
+    "url": "/movie-pratice/static/js/2.16f602cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-pratice/static/js/2.16f602cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee3c20913153477f744",
+    "url": "/movie-pratice/static/js/main.82f68b8e.chunk.js"
+  },
+  {
+    "revision": "c666be38aff6c5a51a0b",
+    "url": "/movie-pratice/static/js/runtime-main.cb4f079d.js"
+  }
+]);
